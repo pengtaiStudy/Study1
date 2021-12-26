0c9ab6c69a277a811f9c81531e87a5d2a45ace45
imim528 <siyou0528@gmail.com>
2021-12-26 19:34:36 +0900

    connect server n browser

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket)=>{
+    console.log("Connected to Browser O");
+    socket.on("close", ()=> {
+        console.log("Disconnected from the Browser X");
+    });
+    socket.on("message", message => {
+        const translatedMsgData = message.toString('utf8');
+        console.log(translatedMsgData);
+    });
+    socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);

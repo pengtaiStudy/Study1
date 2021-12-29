2d791962b2f8f9db32429f8193bd8503f1876176
imim528 <siyou0528@gmail.com>
2021-12-30 08:40:33 +0900

    update: wss toggle block, install SocketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -12,14 +12,10 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose(){
-    console.log("Disconnected from the Browser X");
-}
-
-const sockets = [];
+/* const sockets = [];
 
 wss.on("connection", (socket)=>{
     sockets.push(socket);
@@ -37,6 +33,6 @@ wss.on("connection", (socket)=>{
                 socket["nickname"] = message.payload;
         }
     });
-});
+}); */
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);

1d872058eee6c2b849aac50de63290a255d87dbf
imim528 <siyou0528@gmail.com>
2022-01-18 01:57:15 +0900

    update; chapter 3.6 Answers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -96,17 +96,18 @@ camerasSelect.addEventListener("input", handleCameraChange);
 const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-async function startMedia(){
+async function initCall(){
     welcome.hidden = true;
     call.hidden = false;
     await getMedia();
     makeConnection();
 }
 
-function handlewelcomeSubmit(event){
+async function handlewelcomeSubmit(event){
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
-    socket.emit("join_room", input.value, startMedia);
+    await initCall();
+    socket.emit("join_room", input.value);
     roomName = input.value;
     input.value="";
 
@@ -122,8 +123,16 @@ socket.on("welcome", async ()=>{
     socket.emit("offer", offer, roomName);
 });
 
-socket.on("offer", offer =>{
-    console.log(offer);
+socket.on("offer", async (offer) =>{
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    console.log(answer);
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit("answer", answer);
+});
+
+socket.on("answer", answer =>{
+    myPeerConnection.setRemoteDescription(answer);
 });
 
 // RTC Code
